1f9b0884551488c73873aca2c89c59d60e4761f1
CoinerLo <coiner4677@gmail.com>
2021-09-26 17:34:55 +0300

    Add starting settings

---
+++ bin/slack.js
@@ -0,0 +1,10 @@
+import getApp from '../server/index.js';
+
+const port = process.env.PORT || 5000;
+const address = '0.0.0.0';
+
+getApp({ port }).then((app) => {
+  app.listen(port, address, () => {
+    console.log(`Server has been started on ${port}`);
+  });
+});
